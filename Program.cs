6fb881c5cddb270379363956604e49afa1712660
Simon Glain Nguema <130991545+SimonNguema@users.noreply.github.com>
2023-05-02 23:11:31 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Paie.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Net;
+using System.Threading.Channels;
+using System.Xml.Linq;
+
+
+namespace paie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.Afficher();
+
+
+        }
+    }
+
+}
