32468c88cc23473212e371d5cee023616d055e69
ABAKAR SALEH BICHARA <130989961+ABAKAR23@users.noreply.github.com>
2023-05-21 20:56:13 +0200

    Add files via upload

---
+++ Ajouter_fiches.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Gestion_Paies.Historique_paiement;
+
+namespace Gestion_Paies
+{
+    public class Ajouter_fiches
+    {
+        public void Ajouter_fiche(fiche_paiement fiche)
+        {
+            liste_paiement.Add(fiche);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
-﻿namespace projetFinal
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Enregistrement enregistrement = new Enregistrement();
-            enregistrement.Menu();
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
+namespace Gestion_Paies
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
